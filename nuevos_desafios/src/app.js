afec526f2455921dd2e5e22c8eea3c0a4b1590d2
santilapi13 <santilapiana02@gmail.com>
2023-08-31 13:43:25 -0300

    Planteo desafio 4

---
+++ nuevos_desafios/src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import {router as productsRouter} from './routes/products.router.js';
+import {router as cartsRouter} from './routes/carts.router.js';
+import {router as viewsRouter} from './routes/views.router.js';
+import handlebars from 'express-handlebars';
+
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+app.use("/", viewsRouter);
+
+app.get('*', (req, res) => {
+    res.setHeader("Content-Type", "text/plain");
+    res.status(404).send('error 404 - page not found');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server corriendo en puerto ${PORT}`);
+});
+
