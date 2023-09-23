2e58df351aba0f471b42930b53b4503079071c1b
santilapi13 <santilapiana02@gmail.com>
2023-09-23 14:53:10 -0300

    Refactor de carpetas

--- pre_entrega_01/src/app.js
+++ pre_entrega_01/src/app.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import {router as productsRouter} from './routes/products.router.js';
 import {router as cartsRouter} from './routes/carts.router.js';
+import {router as viewsRouter} from './routes/views.router.js';
+import {router as chatRouter, chatSocket} from './routes/chat.router.js';
+import handlebars from 'express-handlebars';
+import __dirname from './util.js';
+import {Server} from 'socket.io'
+import mongoose from "mongoose"
 
 const PORT = 8080;
 const app = express();
@@ -8,15 +14,35 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.use(express.static(__dirname + '/public'));
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+app.use("/chat", chatRouter);
+app.use("/", viewsRouter);
 
 app.get('*', (req, res) => {
     res.setHeader("Content-Type", "text/plain");
     res.status(404).send('error 404 - page not found');
 });
 
-app.listen(PORT, () => {
+const serverExpress = app.listen(PORT, () => {
     console.log(`Server corriendo en puerto ${PORT}`);
 });
 
+mongoose.connect('mongodb+srv://santilapiana02:aHGwx1LOTFj9kMur@e-commerce.un2yreb.mongodb.net/?retryWrites=true&w=majority')
+    .catch((error) => {
+        console.log("Cannot connect to database " + error);
+        process.exit();
+    });
+
+export const io = new Server(serverExpress);
+
+io.on('connection', (socket) => {
+    console.log(`Client with id ${socket.id} has connected`);
+    chatSocket(io, socket);
+});
