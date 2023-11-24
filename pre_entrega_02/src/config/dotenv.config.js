b661d2fa9b54de12f12aad2b2d9566d532b543b5
santilapi13 <santilapiana02@gmail.com>
2023-11-24 18:16:02 -0300

    Add MODE variable to dotenv config

--- pre_entrega_02/src/config/dotenv.config.js
+++ pre_entrega_02/src/config/dotenv.config.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config({path:"../.env", override:true});
 
 export const config = {
+	MODE: process.env.MODE,
 	PORT: process.env.PORT,
 	MONGO_URL: process.env.MONGO_URL,
 	PRIVATE_KEY: process.env.PRIVATE_KEY,
