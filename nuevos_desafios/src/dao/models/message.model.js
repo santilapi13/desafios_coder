66e778c445ffc5c9f7f547bfb96c3efc19e8a64e
santilapi13 <santilapiana02@gmail.com>
2023-12-14 13:51:07 -0300

    Refactor directorios

--- nuevos_desafios/src/dao/models/message.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const messageCollection = "message";
-const messageSchema = new mongoose.Schema({
-    user: String,
-    message: String
-});
-
-export const messageModel = mongoose.model(messageCollection, messageSchema);
